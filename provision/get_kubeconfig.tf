f4f9d516ef1ff49650723f3169b24f7407f43781
Arvind Bhoj <abhoj@dev.azure.com>
2021-10-28 01:01:50 -0400

    Added missing tf file for downloading kubeconfig

---
+++ provision/get_kubeconfig.tf
@@ -0,0 +1,13 @@
+resource "local_file" "get_kubeconfig" {
+  filename = "get_kubeconfig.sh"
+
+  depends_on = [aws_instance.registry]
+
+  provisioner "local-exec" {
+    command = "chmod 700 get_kubeconfig.sh"
+  }
+  content = <<EOF
+scp -i ${trimprefix(var.ssh_private_key_file, "../")} centos@${aws_instance.registry[0].public_ip}:/home/centos/admin.conf ${var.cluster_name}-admin.conf
+exit
+EOF
+}
