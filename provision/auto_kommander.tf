24ff3880afc30f8beed44e600231eaf247c6fb86
Arvind Bhoj <abhoj@dev.azure.com>
2021-11-03 13:16:41 -0400

    Reduced sleep time for kommader releases to rollout from 15 mins to 1o minutes

--- provision/auto_kommander.tf
+++ provision/auto_kommander.tf
@@ -20,8 +20,8 @@ if [ $# -ne 0 ]; then
       helm repo update
       helm install -n kommander --create-namespace kommander-bootstrap kommander/kommander-bootstrap --version=${var.kommander_version} --set certManager=$(kubectl get ns cert-manager > /dev/null 2>&1 && echo "false" || echo "true")
       echo -e "\n\nKommander helm chart deployed. Might take upto 30 minutes for the addon apps to be deployed"
-      echo -e "\nWaiting for helmreleases to be rolled out"
-      sleep 900 #Sleeping 15 minutes
+      echo -e "\nWaiting for helmreleases to be rolled out. Sleeping 10 minutes" 
+      sleep 600
       echo -e "\nkubectl -n kommander wait --for condition=Released helmreleases --timeout 1s --all"  
       kubectl -n kommander wait --for condition=Released helmreleases --timeout 1s --all
       echo -e "\nConnect to the bootstrap server and wait for all Helm Release resources to be ready"
