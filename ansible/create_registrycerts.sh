c201722add646d9f0ca0d90a9b77f4de7f633628
arbhoj <arbhoj@icloud.com>
2024-01-02 11:15:09 -0500

    Added step to generate certs and make the local registry https enabled

--- ansible/create_registrycerts.sh
+++ ansible/create_registrycerts.sh
@@ -5,4 +5,5 @@ openssl genrsa -out ca.key 4096
 openssl req -x509 -new -nodes -key ca.key -sha256 -days 1024 -out ca.crt -subj "/CN=dockerroot"
 openssl genrsa -out client-key.key 2048 
 openssl req -new -key client-key.key -subj /CN=`hostname -i` -out client.csr
-openssl x509 -req --extfile <(printf "subjectAltName=DNS:`hostname -i`") in client.csr -CA ca.crt -CAkey ca.key -CAcreateserial -out client-cert.crt
+#openssl x509 -req --extfile <(printf "subjectAltName=DNS:`hostname -i`") in client.csr -CA ca.crt -CAkey ca.key -CAcreateserial -out client-cert.crt
+openssl x509 -req -in client.csr -CA ca.crt -CAkey ca.key -CAcreateserial -out client-cert.crt
